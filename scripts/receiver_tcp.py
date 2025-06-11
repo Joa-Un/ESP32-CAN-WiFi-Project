726317aa642a3a1aa6ec2ed069ca02fd0909a907
Joakim Unha <joakim.unha@tuni.fi>
2025-06-11 14:18:44 +0300

    working version where PC disconnects after receiving data and reconnects

--- scripts/receiver_tcp.py
+++ scripts/receiver_tcp.py
@@ -1,20 +1,18 @@
-# Code for a PC receiving data from an AP ESP32
-
 import socket
 import time
 
 ESP32_IP = "192.168.1.1"  # ESP32 AP IP address
-TCP_PORT = 4210
+TCP_PORT = 4210 # Port number to connect to the ESP32
 
 def loop():
     while True:
         try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: # Create a TCP socket
-                sock.settimeout(10) # Set a timeout for the connection attempt
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.settimeout(10)
                 print(f"Connecting to {ESP32_IP}:{TCP_PORT}")
                 sock.connect((ESP32_IP, TCP_PORT)) # Connect to the ESP32
-                
-                # Print received data from the ESP32
+
+                # Receive data from the ESP32
                 data = sock.recv(2048)
                 print("Received:", data.decode())
 
@@ -22,7 +20,8 @@ def loop():
         except (socket.timeout, ConnectionRefusedError) as e: # Handle timeout or connection refused
             print("No response or connection refused:", e)
 
-        time.sleep(1) # Wait before retrying
+        time.sleep(1) # Wait before trying again
+
 
 if __name__ == "__main__":
     loop()

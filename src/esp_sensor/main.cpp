3bb8b2e87ef723fba4a059acb83263fdae7fa7da
Joakim Unha <joakim.unha@tuni.fi>
2025-06-12 09:30:41 +0300

    file structure changes

--- src/esp_sensor/main.cpp
+++ src/esp_sensor/main.cpp
@@ -1,3 +1,10 @@
 // Code for generating sensor data and sending it over CAN to a AP ESP32, which sends it to a PC over TCP/IP.
 
 #include <Arduino.h>
+void setup() {
+  // alustuskoodi
+}
+
+void loop() {
+  // pääsilmukka
+}
